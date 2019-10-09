a7d94e587f6b605231e1479e3504d4aec6de6189
Fred Vassard <fred@meraki.com>
2019-10-09 09:06:15 -0700

    Adding ssh config stuff

--- DotFiles/zsh/env/env.zsh
+++ DotFiles/zsh/env/env.zsh
@@ -7,4 +7,9 @@ if [[ $CURRENT_OS == 'OS X' ]]; then
 elif [[ $CURRENT_OS == 'Linux' ]]; then
     # Linux Env Variables
 fi
-
+if type "keychain" > /dev/null; then
+    for k in `grep -l PRIVATE ~/.ssh/* 2>/dev/null`; do keychain --inherit any --quiet $k; done
+    [ -z "$HOSTNAME" ] && HOSTNAME=`uname -n`
+    . $HOME/.keychain/$HOSTNAME-sh
+    [ -f $HOME/.keychain/$HOSTNAME-sh-gpg ] && . $HOME/.keychain/$HOSTNAME-sh-gpg
+fi
